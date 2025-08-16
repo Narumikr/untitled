fd095d6f428ed228b862b3e71ff4fb7a1a9a407c
Narumikr <narunarumikumiku396@gmail.com>
2025-08-16 14:37:17 +0900

    incorporate plopjs (#56)
    * incorporate plopjs
    
    * revert package lock
    
    * npm install plop

---
+++ plopfile.js
@@ -0,0 +1,34 @@
+export default function (plop) {
+  plop.setGenerator('component', {
+    description: 'Create a React component',
+    prompts: [
+      {
+        type: 'input',
+        name: 'folder',
+        message: 'Folder name?'
+      },
+      {
+        type: 'input',
+        name: 'component',
+        message: 'Component name?'
+      }
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: 'src/components/{{kebabCase folder}}/{{pascalCase component}}.tsx',
+        templateFile: 'plop-templates/component.tsx.hbs'
+      },
+      {
+        type: 'add',
+        path: 'src/components/{{kebabCase folder}}/{{pascalCase component}}.module.scss',
+        templateFile: 'plop-templates/component.module.scss.hbs'
+      },
+      {
+        type: 'add',
+        path: 'stories/{{kebabCase folder}}/{{pascalCase component}}.stories.tsx',
+        templateFile: 'plop-templates/component.stories.tsx.hbs'
+      }
+    ]
+  })
+}
