14b56ebfcd8cd56c4d008c9e1ca7d8c7e1e44ddf
Narumikr <narunarumikumiku396@gmail.com>
2025-09-18 22:16:08 +0900

    upd8 provider

--- src/components/provider/YourSekaiProvider.tsx
+++ src/components/provider/YourSekaiProvider.tsx
@@ -1,13 +1,17 @@
-import React, { createContext, useMemo, useState } from 'react'
+import React, { createContext, useEffect, useMemo, useState } from 'react'
 
+import { useLocalStorage } from '@/hooks/useLocalStorage'
 import { DARK_MODE } from '@/hooks/useThemeMode'
 
+import type { PaletteMode } from '@/hooks/useThemeMode'
 import type { ColorsSekaiKey } from '@/styles/sekai-colors'
 import type { SekaiTheme } from '@/utils/createSekai'
 
+export const YOUR_COLOR_THEME = 'your_color_theme'
 export interface YourSekaiContextProps {
   sekaiTheme: SekaiTheme
   onSwitchSekaiColor: (sekai: ColorsSekaiKey) => void
+  onSwitchColorTheme: (color: PaletteMode) => void
 }
 
 export const YourSekaiContext = createContext<YourSekaiContextProps | null>(null)
@@ -15,9 +19,24 @@ export const YourSekaiContext = createContext<YourSekaiContextProps | null>(null
 export interface YourSekaiProviderProps {
   children: React.ReactNode
   sekaiTheme: SekaiTheme
+  settingOptions?: {
+    /**
+     * Whether to store the color theme setting in LocalStorage
+     */
+    colorTheme?: boolean
+  }
 }
-export const YourSekaiProvider = ({ children, sekaiTheme }: YourSekaiProviderProps) => {
+export const YourSekaiProvider = ({
+  children,
+  sekaiTheme,
+  settingOptions
+}: YourSekaiProviderProps) => {
   const [currentSekaiTheme, setCurrentSekaiTheme] = useState<SekaiTheme>(sekaiTheme)
+  const {
+    storedvalue: colorTheme,
+    setStoredValue: setColorTheme,
+    deleteLocalStorage
+  } = useLocalStorage<PaletteMode>(YOUR_COLOR_THEME, sekaiTheme.palette.mode)
 
   const onSwitchSekaiColor = (sekai: ColorsSekaiKey) => {
     setCurrentSekaiTheme((pre) => ({
@@ -29,19 +48,53 @@ export const YourSekaiProvider = ({ children, sekaiTheme }: YourSekaiProviderPro
     }))
   }
 
+  const onSwitchColorTheme = (color: PaletteMode) => {
+    setColorTheme(color)
+    setCurrentSekaiTheme((pre) => ({
+      ...pre,
+      palette: {
+        ...pre.palette,
+        palette: {
+          mode: color
+        }
+      }
+    }))
+  }
+
+  useEffect(() => {
+    setCurrentSekaiTheme((pre) => ({
+      ...pre,
+      palette: {
+        ...pre.palette,
+        mode: colorTheme
+      }
+    }))
+  }, [colorTheme])
+
+  useEffect(() => {
+    return () => {
+      // Delete colorTheme store value, if setting option is not available
+      if (!settingOptions?.colorTheme) {
+        deleteLocalStorage()
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
   const provideValue = {
     sekaiTheme: currentSekaiTheme,
-    onSwitchSekaiColor
+    onSwitchSekaiColor,
+    onSwitchColorTheme
   }
 
   const globalStyle = useMemo(
     () => `
     * {
-      font-family: ${sekaiTheme.typography?.fontFamily};
+      font-family: ${sekaiTheme.typography.fontFamily};
     }
     body {
-      color: ${sekaiTheme.palette?.mode === DARK_MODE ? '#e0e0e0' : '#212121'};
-      background: ${sekaiTheme.palette?.mode === DARK_MODE ? '#121212' : '#ffffff'};
+      color: ${sekaiTheme.palette.mode === DARK_MODE ? '#e0e0e0' : '#212121'};
+      background: ${sekaiTheme.palette.mode === DARK_MODE ? '#121212' : '#ffffff'};
     }
   `,
     [sekaiTheme]
