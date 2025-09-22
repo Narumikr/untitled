afc5af0e373716fa5b96f7184dbac2ae130a6840
Narumikr <narunarumikumiku396@gmail.com>
2025-09-22 21:08:57 +0900

    create icon images!

---
+++ stories/card/MusicBannerCard.stories.tsx
@@ -0,0 +1,83 @@
+import { MusicBannerCard } from '@/components/card/MusicBannerCard'
+
+import { COLORS_SEKAI_KEYS } from '@/styles/sekai-colors'
+
+import type { Meta, StoryObj } from '@storybook/react'
+
+const meta = {
+  title: 'Untitled/MusicBannerCard',
+  component: MusicBannerCard,
+  decorators: [],
+  parameters: {},
+  tags: ['autodocs'],
+  argTypes: {
+    id: {
+      description: 'Unique Id',
+      table: {
+        type: { summary: 'string' }
+      }
+    },
+    className: {
+      description: 'Custom styles',
+      table: {
+        type: { summary: 'string' }
+      },
+      control: false
+    },
+    style: {
+      description: 'Style object',
+      table: {
+        type: { summary: 'React.CSSProperties' }
+      },
+      control: false
+    },
+    sekai: {
+      description: 'What SEKAI color to use',
+      table: {
+        type: { summary: 'ColorsSekaiKey' },
+        defaultValue: { summary: 'Miku' }
+      },
+      control: { type: 'select' },
+      options: [...Object.keys(COLORS_SEKAI_KEYS)]
+    },
+    themeMode: {
+      description: 'Light or Dark mode',
+      table: {
+        type: { summary: 'PaletteMode' },
+        defaultValue: { summary: 'light' }
+      },
+      control: { type: 'select' },
+      options: ['light', 'dark']
+    }
+  },
+  args: {}
+} satisfies Meta<typeof MusicBannerCard>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const DefaultLight: Story = {
+  args: {
+    id: 'music-banner-card-default-light',
+    sekai: 'Miku',
+    themeMode: 'light',
+    selected: false
+  },
+  parameters: {
+    sekai: 'Miku',
+    background: 'light'
+  }
+}
+
+export const DefaultDark: Story = {
+  args: {
+    id: 'music-banner-card-default-dark',
+    sekai: 'Miku',
+    themeMode: 'dark',
+    selected: false
+  },
+  parameters: {
+    sekai: 'Miku',
+    background: 'dark'
+  }
+}
