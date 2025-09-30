7610a399cd9f104d66acc5bf3e23b47d60fb8ed9
Narumikr <narunarumikumiku396@gmail.com>
2025-09-30 20:42:48 +0900

    add test yaml

--- .github/script/prsk-labeling-logic.js
+++ .github/script/prsk-labeling-logic.js
@@ -29,7 +29,7 @@ export const createLabelText = (character) => {
   const today = new Date()
   const month = String(today.getMonth() + 1).padStart(2, '0')
   const day = String(today.getDate()).padStart(2, '0')
-  return `${month}/${day}: ${character.name}`
+  return `${month}/${day} - ${character.name}`
 }
 
 // Collaboration comment
