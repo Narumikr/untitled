510b59540e720a369e687d8b42795b8ad74ee166
Narumikr <narunarumikumiku396@gmail.com>
2025-10-01 12:40:32 +0900

    MarqueeTextコンポーネントの作成 (#81)

---
+++ src/internal/color.constant.ts
@@ -0,0 +1,12 @@
+/**
+ * Color constants for light and dark modes.
+ * If global.css is changed, please update these values accordingly.
+ */
+
+// provider text color
+export const COLOR_LIGHT_MODE = '#212121'
+export const COLOR_DARK_MODE = '#e0e0e0'
+
+// provider background color
+export const BACKGROUND_LIGHT_MODE = '#ffffff'
+export const BACKGROUND_DARK_MODE = '#121212'
