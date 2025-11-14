1b3878d62ddf9c7bf062307ec80cc40f742d701f
Narumikr <narunarumikumiku396@gmail.com>
2025-11-15 04:19:15 +0900

    Refactor index generator

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,8 @@
+/**
+ * This file is auto-generated. Do not edit manually.
+ * Generated at: 2025-11-15 04:18:14
+ */
+
 export * from './useCreateSekai'
 export * from './useCurrectTime'
 export * from './useLocalStorage'
